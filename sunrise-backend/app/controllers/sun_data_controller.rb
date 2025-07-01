3d23da8b157fd38f20dbfc9efb133e30e4e36ac9
Daniel <38920805+DaaanielBrandao@users.noreply.github.com>
2025-07-01 21:22:35 +0100

    sunrisesunset api service

--- sunrise-backend/app/controllers/sun_data_controller.rb
+++ sunrise-backend/app/controllers/sun_data_controller.rb
@@ -4,26 +4,44 @@ class SunDataController < ApplicationController
     start_date = Date.parse(params[:start_date])
     end_date = Date.parse(params[:end_date])
 
-    days = (start_date..end_date).map do |date|
-      {
-        date: date,
-        location: location,
-        sunrise: "06:00",
-        sunset: "20:30",
-        golden_hour: {
-          morning: "05:30 - 06:30",
-          evening: "19:30 - 20:30"
-        }
-      }
-    end
+    #temp
+    lat = 38.907192
+    lng = -77.036873
 
-    render json: {
-      location: location,
-      from: start_date,
-      to: end_date,
-      data: days
-    }
+    api_service = SunriseSunsetApiService.new(
+      lat: lat,
+      lng: lng,
+      start_date: start_date,
+      end_date: end_date
+    )
+
+    api_response = api_service.fetch_data
+
+    if api_response["status"] == "OK"
+      render json: format_response(api_response)
+    else
+      render json: { error: "API Error" }, status: :service_unavailable
+    end
   rescue ArgumentError
-    render json: { error: "Invalid dates" }, status: :bad_request
+    render json: { error: "Invalid parameters" }, status: :bad_request
+  end
+
+  private def format_response(api_response)
+    results = api_response["results"]
+    {
+      from: results.first["date"],
+      to: results.last["date"],
+      data: results.map do |day|
+        {
+          date: day["date"],
+          sunrise: day["sunrise"],
+          sunset: day["sunset"],
+          golden_hour: {
+            morning: "#{day['dawn']} - #{day['sunrise']}",
+            evening: "#{day['golden_hour']} - #{day['dusk']}"
+          }
+        }
+      end
+    }
   end
 end
---
+++ sunrise-backend/app/services/sunrise_sunset_api_service.rb
@@ -0,0 +1,27 @@
+﻿class SunriseSunsetApiService
+  include HTTParty
+  base_uri 'https://api.sunrisesunset.io'
+
+  def initialize(lat:, lng:, start_date: nil, end_date: nil)
+    @lat = lat
+    @lng = lng
+    @start_date = start_date
+    @end_date = end_date
+  end
+
+  def fetch_data
+    response = self.class.get('/json', query: query_params)
+    JSON.parse(response.body)
+  end
+
+  private
+
+  def query_params
+    {
+      lat: @lat,
+      lng: @lng,
+      date_start: @start_date,
+      date_end: @end_date
+    }.compact
+  end
+end
